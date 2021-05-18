41f5a714b6cfadb58f5f281b2bf307ad5dec42ff
Maksim Stseshanok <maksim.stseshanok@gmail.com>
2021-05-18 12:29:36 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { Counter } from './features/counter/Counter';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
